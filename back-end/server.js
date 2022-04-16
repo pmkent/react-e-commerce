dfdfca6c18f3454af3be5bad5d2aa7fc33b24eb3
Phil Kieti <philmkieti@hotmail.com>
2022-04-15 20:48:46 -0400

    08 Fetched Products from Backend using Axios

--- back-end/server.js
+++ back-end/server.js
@@ -1,7 +1,12 @@
 import express from 'express';
 import data from './data.js';
+import cors from 'cors';
 
 const app = express();
+// app.use(cors());
+app.use(cors({
+    origin: ['http://localhost:5000', 'http://localhost:3000']
+}));
 
 app.get('/api/products', (req, res) => {
     res.send(data.products);
