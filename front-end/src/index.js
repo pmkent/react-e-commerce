cc62a4d07ec525fad62564e9b08b0284a6adf07e
Phil Kieti <philmkieti@hotmail.com>
2022-04-18 12:13:02 -0400

    Added boostrap ui, restyled using bootstrap

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css'; // making amazona header look great? 4-18-22
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
